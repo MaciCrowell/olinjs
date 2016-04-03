9650fb2de84bb1e96cc03d08ac9604e504bbaf38
Sarah Walters <swalters4925@gmail.com>
2016-04-02 22:47:26 -0400

    Homework 8 feedback

--- lessons/07-apis-debugging/homework/tests/server/appSpec.js
+++ lessons/07-apis-debugging/homework/tests/server/appSpec.js
@@ -5,15 +5,7 @@ describe("The app", function() {
 	it('should return 200 OK on GET /', function(done) {
 		request(app)
 		.get('/')
-		.expect(200)
-		.end(function(err, res) {
-	        // Supertest lets us end tests this way...
-	        // (useful if we want to check a couple more things with chai)
-	        if (err) {
-	        	return done(err);
-	        }
-	        done();
-	    });
+		.expect(200, done); // nicer shorthand if you're not doing anything in the "end"
 	});
 
 	it('should return 200 OK on GET /login', function(done) {
@@ -30,4 +22,9 @@ describe("The app", function() {
 	    });
 	});
 
+	// Completely understand if you don't want to copy-paste a statement a bunch of
+	// times to cover all of the routes -- as long as you understand that you probably would
+	// do something like that if you were testing an app for production.
+
+	// Probably a good idea to try to trigger a couple of error cases, also.
 });
--- lessons/07-apis-debugging/homework/tests/server/models/twoteModelSpec.js
+++ lessons/07-apis-debugging/homework/tests/server/models/twoteModelSpec.js
@@ -8,7 +8,7 @@ var Twote = require('./../../../models/twoteModel');
 describe('Twote Model', function() {
 
  	 it('should create a new twote', function(done) {
- 		
+
  		var twote = new Twote({
  			text: 'Cheese',
  			user: mongoose.Types.ObjectId('4edd40c86762e0fb12000003')
@@ -22,7 +22,7 @@ describe('Twote Model', function() {
  			}
  		});
  	});
- 	
+
 	it('should remove all ingredients', function(done) {
  		Twote.findOneAndRemove({text: 'Cheese'},function(err,removed) {
  			if (err) {
@@ -33,6 +33,20 @@ describe('Twote Model', function() {
  			}
  			done();
  		});
-	}); 	
+	});
 
 });
+
+/* Structurally, this looks good to me! I like that your describe(...) includes an it(...)
+   for adding an object and an it(...) for removing the same object... it's generally good practice
+   for a test to "clean up after itself" -- leave the database unmodified.
+
+   With that in mind -- I think I might not have picked "Cheese" as the name of the ingredient
+   to try saving... seems reasonably likely that an ingredient by that name might already
+   exist in whatever database you're hooked up to, and you might end up deleting both the existing one
+   and the new one with your Ingredient.remove(...) call. Fortunately it doesn't really matter whether the name
+   of the ingredient you're trying to save makes semantic sense in the context of a burger restaurant or not...
+   so, here's what I've seen people do in cases like this: come up with some long, fairly-likely-to-be-unique
+   string using the current unix epoch time, or a random number generator, or both combined. Not a big deal in
+   this context, but safer in production.
+*/
